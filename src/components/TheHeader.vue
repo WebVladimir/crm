b2fef5ee526c25d45d500ec8f1869fc450ecae0f
Vladimir <vova.drime@gmail.com>
2023-05-01 17:36:48 +0300

    style: header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,26 +1,15 @@
 <template>
-  <a-layout-header class="the-header">
-    <div class="the-header__block">
+  <a-layout-header class="the-header flex z-10 justify-between min-h-[70px]">
+    <div class="flex items-center">
       <app-burger />
       <app-date class="ml-[30px]" />
     </div>
-    <div class="the-header__block" />
+    <div class="flex items-center" />
   </a-layout-header>
 </template>
 
 <style lang="scss">
 .the-header {
-  z-index: 1;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  min-height: 70px;
   padding: 0 20px;
-}
-
-.the-header__block {
-  display: flex;
-  align-items: center;
 }
 </style>
