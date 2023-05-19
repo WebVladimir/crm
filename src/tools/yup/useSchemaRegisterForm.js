4d9e12bcd542d4d7e19d3b5d24de04ef61465d40
Vladimir <vova.drime@gmail.com>
2023-05-20 02:00:08 +0300

    feat: form
    перенес все формы в scheme - FormVueLate

---
+++ src/tools/yup/useSchemaRegisterForm.js
@@ -0,0 +1,24 @@
+import AppInput from "~/components/App/AppInput.vue"
+import AppInputPassword from "~/components/App/AppInputPassword.vue"
+
+export const useSchemaRegisterForm = () => {
+  markRaw(AppInput)
+  markRaw(AppInputPassword)
+
+  const schema = shallowRef({
+    email: {
+      component: AppInput,
+      placeholder: "Введите email",
+      name: "email",
+      label: "Email",
+    },
+    password: {
+      component: AppInputPassword,
+      placeholder: "Введите пароль",
+      name: "password",
+      label: "Пароль",
+    },
+  })
+
+  return { schema }
+}
